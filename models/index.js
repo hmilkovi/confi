d06e74ae510feac3b7e8bfba9a7b464e5c8aae9e
Hrvoje Milkovic <hrvoje@krakensystems.co>
2019-12-03 10:31:40 +0100

    cleanup, dockerignore, pg auth

--- models/index.js
+++ models/index.js
@@ -1,12 +1,7 @@
 const admin = require('./hcData');
-const pgp = require('pg-promise')(/* initialization options */);
+const pgp = require('pg-promise')();
 
-const db = pgp({
-    database: 'confi',
-    port: 5432,
-    user: 'confi', // any admin user
-    password: 'xpzTwqUuSlcg'
-});
+var db = pgp(process.env.DB_CONNECTION);
 
 var data = {};
 
